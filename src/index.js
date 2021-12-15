05020745503e07b0d9363b4308d53dccba0a2ee7
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2021-12-15 11:31:48 -0300

    Update part1: 1.5 course information step5

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 const Header = ({ course }) => {
   return (
-    <h1>{course}</h1>
+    <h1>{course.name}</h1>
   );
 }
 
@@ -16,8 +16,6 @@ const Part = (props) => {
 }
 
 const Content = ({ parts }) => {
-  console.log(parts);
-  console.log(parts[0].name)
   return (
     <div>
       <Part
@@ -43,27 +41,29 @@ const Total = ({ parts }) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  } 
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 }

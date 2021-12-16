2de0de8e347e308625ac8b09915fa258ea011459
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2021-12-16 14:16:07 -0300

    Update part1: 1.7 unicafe step 2

--- src/index.js
+++ src/index.js
@@ -10,7 +10,16 @@ const Button = (props) =>
   </button>
 
 const Stat = (props) => 
-  <p>{props.opinion}:{props.number}</p>
+  <p>{props.opinion}: <strong>{props.number}</strong></p>
+
+const Total = (props) =>
+  <p>all: <strong>{props.all}</strong></p>
+
+const Average = (props) =>
+  <p>average: <strong>{props.avg}</strong></p>
+
+const Positive = (props) =>
+  <p>positive: <strong>{props.rate}%</strong></p>
 
 const App = () => {
   // save clicks of each button to its own state
@@ -21,6 +30,22 @@ const App = () => {
   const goodHanlder = () => setGood(good + 1);
   const neutralHandler = () => setNeutral(neutral + 1);
   const badHandler = () => setBad(bad + 1);
+
+  const total = good+neutral+bad; 
+
+  let goodRate = 0;
+  (total === 0) ? goodRate = 0 : goodRate = good * 100 / total;
+  let badRate = 0;
+  (total === 0) ? badRate = 0 : badRate = bad * 100 / total;
+
+  let avg;
+  if (good === 0 && neutral === 0 && bad === 0) {
+    avg = 0;
+  } else if (good > bad) {
+    avg = ((goodRate - ((neutral * 100 / total) / 2)) / 100);
+  } else {
+    avg = ((-badRate + ((neutral * 100 / total) / 2)) / 100);
+  }
 
   return (
     <div>
@@ -33,6 +58,9 @@ const App = () => {
       <Stat opinion='good' number={good} />
       <Stat opinion='neutral' number={neutral} />
       <Stat opinion='bad' number={bad} />
+      <Total all={total} />
+      <Average avg={avg} />
+      <Positive rate={goodRate} />
     </div>
   )
 }

d5615b3d721bee4758c70efc39a6cdde8cd68e9f
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2021-12-16 15:27:20 -0300

    Add new component: Statistics Also, we added new functions to keep the code cleaner

--- src/index.js
+++ src/index.js
@@ -21,6 +21,20 @@ const Average = (props) =>
 const Positive = (props) =>
   <p>positive: <strong>{props.rate}%</strong></p>
 
+const Statistics = (props) => {
+  return (
+    <div id='statistics'>
+      <H2 text='Statistics' />
+      <Stat opinion='good' number={props.goodOp} />
+      <Stat opinion='neutral' number={props.neutralOp} />
+      <Stat opinion='bad' number={props.badOp} />
+      <Total all={props.all} />
+      <Average avg={props.avg} />
+      <Positive rate={props.rate} />
+    </div>
+  );
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -31,8 +45,29 @@ const App = () => {
   const neutralHandler = () => setNeutral(neutral + 1);
   const badHandler = () => setBad(bad + 1);
 
-  const total = good+neutral+bad; 
+  const total = good+neutral+bad;
+  let avg = calculateAvg(good, neutral, bad, total);
+  let goodRate = calculateGoodRate(good, total);
 
+  return (
+    <div>
+      <H2 text='Give feedback' />
+      <Button handler={goodHanlder} text='good' />
+      <Button handler={neutralHandler} text='neutral' />
+      <Button handler={badHandler} text='bad' />
+      <Statistics 
+        goodOp={good}
+        neutralOp={neutral}
+        badOp={bad}
+        all={total}
+        avg={avg}
+        rate={goodRate}  
+      />
+    </div>
+  )
+}
+
+function calculateAvg(good, neutral, bad, total) {
   let avg;
   if (good === 0 && neutral === 0 && bad === 0) {
     avg = 0;
@@ -42,25 +77,14 @@ const App = () => {
     avg = ((neutral - 2 * bad) / (2 * total));
   }
 
-  let goodRate = 0;
-  (total === 0) ? goodRate = 0 : goodRate = good * 100 / total;
+  return avg;
+}
 
-  return (
-    <div>
-      <H2 text='Give feedback' />
-      <Button handler={goodHanlder} text='good' />
-      <Button handler={neutralHandler} text='neutral' />
-      <Button handler={badHandler} text='bad' />
-      
-      <H2 text='Statistics' />
-      <Stat opinion='good' number={good} />
-      <Stat opinion='neutral' number={neutral} />
-      <Stat opinion='bad' number={bad} />
-      <Total all={total} />
-      <Average avg={avg} />
-      <Positive rate={goodRate} />
-    </div>
-  )
+function calculateGoodRate(good, total) {
+  let rate;
+  (total === 0) ? rate = 0 : rate = good * 100 / total;
+  
+  return rate;
 }
 
 ReactDOM.render(<App />, 
